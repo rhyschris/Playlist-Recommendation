54be3029305d0ee31d5354f258ec68617d185824
Christopher Billovits <cjbillov@stanford.edu>
2015-11-07 12:47:36 -0800

    Added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+Project for CS229.
+
---
+++ data/scripts/scraper.py
@@ -0,0 +1,11 @@
+__author__ = "rhyschris"
+''' 
+   Scrapes for commercially licensed music, for use with unsupervised
+   feature learning. 
+
+   Does nothing yet.
+'''
+
+
+if __name__ = '__main__':
+    print 'hello world'
