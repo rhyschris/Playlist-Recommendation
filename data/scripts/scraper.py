c26ba353b061d7c447d06595f49923c48da7e8f5
Rahul Prabala <rprabala@stanford.edu>
2015-11-15 16:20:31 -0800

    changed to use external hard drive

--- data/scripts/scraper.py
+++ data/scripts/scraper.py
@@ -13,9 +13,9 @@ import multiprocessing.dummy as mp
 _public_key = None
 _api_entry = "http://api.soundcloud.com"
 
-""" 
+"""
    Scrapes for commercially licensed music, for use with unsupervised
-   feature learning. 
+   feature learning.
 
    Does nothing yet.
 """
@@ -25,18 +25,18 @@ def download_from_uri(arg):
 
     uri, title = arg
     filepath = "../raw/{0}".format(title)
-    proc = subprocess.Popen("wget --content-disposition " + 
+    proc = subprocess.Popen("wget --content-disposition " +
                                "{0}?client_id={1}".format(uri, _public_key), shell=True)
     proc.wait()
-                      
+
 def get_tracks(pool, count, query):
     print _public_key
     client = soundcloud.Client(client_id=_public_key)
     tracks = client.get('/tracks', q=query, limit=count, license='cc-by')
-    
+
     args = [(track.download_url, track.title) for track in tracks if track.download_url]
     for arg in args:
-        print "{0}: Download from {1}".format(arg[1].encode("utf-8"), 
+        print "{0}: Download from {1}".format(arg[1].encode("utf-8"),
                                               arg[0])
 
     try:
@@ -47,18 +47,18 @@ def get_tracks(pool, count, query):
         return
 
 if __name__ == '__main__':
-    
+
     # default query
-    query = "fetty wap"
-    
+    query = "blue stahli"
+
     if len(sys.argv) > 1:
         query = sys.argv[1]
     with open("API_KEY", 'r') as f:
         _public_key = f.readline().rstrip()
-    
+
     # Change to the "raw" directory for wget a
     # for more seamless use of --content-disposition
-    os.chdir("../raw")
+    os.chdir("/media/Elements/raw")
     get_tracks (mp.Pool(4), 50, query)
-    os.chdir("../scripts")
-    
+    #os.chdir("../scripts")
+
