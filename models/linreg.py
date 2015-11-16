61b45c8243c607f9d2db50fe84ae033eb700e418
Christopher Billovits <cjbillov@stanford.edu>
2015-11-15 17:22:36 -0800

    Modularized linreg.py

--- models/linreg.py
+++ models/linreg.py
@@ -3,6 +3,59 @@ import sys
 import math
 from sklearn import linear_model
 
+# A classifier is something with a train and predict method.
+class Classifier():
+    
+    def train(self, x, y, num_feat, epochs=None):
+        # Calculate objectives
+        if self.epochs is None:
+            self.epochs = epochs if epochs else 20
+
+        self.theta = np.zeros(num_feat + 1)
+        for it in range(self.epochs):
+            alph = self.alpha()
+            for i in range(len(y)):
+                predict = self.predictor(x[i])
+                if predict != y[i]:
+                    self.theta -= alph * self.df_theta(y[i], x[i])
+        return self.theta
+    ''' Runs the predictor on the test set and returns the error rate. '''
+
+    def alpha(iter = 1):
+        return 0.01
+
+    def test(self, x, y):
+        y_hat = np.zeros(len(data))
+    
+        error = 0
+        for i in range(y.size):
+            y_hat[i] = self.predictor(x[i])
+            if y_hat[i] != y[i]:
+                error += 1
+
+        print error / (1.0 * y.size)
+        return error / (1.0 * y.size)
+
+    ''' Runs the class predictor.  Outputs a y label '''
+    def predictor(self, x_i):
+        pass
+    ''' Computes the gradient of the loss with respect to theta. '''
+    def df_theta(self, y_i, x_i):
+        pass
+
+class LogisticClassifier(Classifier):
+    def __init__(self, epochs=None):
+        self.epochs = epochs if epochs else 20
+
+    def predictor(self, x_i):
+        return 1 if self.logistic(np.dot(self.theta, x_i)) > 0.5 else 0
+
+    def df_theta(self, y_i, x_i):
+        prod = self.logistic(np.dot(self.theta, x_i))
+        return (prod - y_i) * x_i
+
+    def logistic(self, z):
+        return 1.0 / (1.0 + np.exp(-1.0 * z))
 
 """ Implements linear regression.  Not bad. """
 
@@ -28,12 +81,12 @@ def featurize(data, vocab):
     for ex in data:
         vec = np.zeros(n + 1)
         for w in ex.split(" "):
-            vec[ vocab[w] + 1 ] += 1
+            val = vocab[w] if w in vocab else -1
+            vec[val + 1] += 1
         vec[0] = 1
         new_data.append(vec)
         
     return new_data
-
 
 def load_data(filename):
 
@@ -52,64 +105,20 @@ def load_data(filename):
             data[i] = second
 
     return data, labels
-        
-''' Binary logistic regression '''
-def output(theta, x):
-    return 1 if logistic(np.dot(theta, x)) > 0.5 else 0
-
-''' The output '''
-def df(y, theta, x):
-    prod = logistic(np.dot(theta, x))
-    return (prod - y) * x
-
-def logistic(z):
-    return 1.0 / (1.0 + np.exp(-1.0 * z))
-
-def train(data, labels, num_feat):
-    # Calculate objectives
-
-    epochs = 20
-    theta = np.zeros(num_feat + 1)
-    for it in range(epochs):
-
-        alpha = 0.01
-        for i in range(len(labels)):
-            x_i = data[i]
-            y_i = labels[i]
-
-            predict = output(theta, x_i)
-            if predict != y_i:
-                theta -= alpha * df(y_i, theta, x_i)
-
-    return theta
-
-def predict(data, theta):
-    y = np.ones(len(data))
-    
-    for i in range(y.size):
-        x_i = data[i]
-        res = logistic(np.dot(theta, x_i))
-        if res < 0.5:
-            y[i] = 0 
-
-    return y
-        
-
-''' Takes two numpy arrays '''
-def test(x, y):
-    diff = int(sum(abs(x - y)))
-    print "Error: ", diff / (1.0 * y.shape[0])
-    
 
 if __name__ == "__main__":
     data, labels = load_data("polarity.train")
     vocab = vocabulary(data)
     vectors = featurize(data, vocab)
-    theta = train(vectors, labels, len(vocab))
-    y = predict(vectors, theta)
-
-    test(labels, y)
 
 
+    reg = LogisticClassifier();
+    reg.train(vectors, labels, len(vocab))
+    print "train"
+    reg.test(vectors, labels)
 
+    print "dev"
+    d, l = load_data("polarity.dev")
+    dev_vec = featurize(d, vocab)
 
+    reg.test(dev_vec, l)
