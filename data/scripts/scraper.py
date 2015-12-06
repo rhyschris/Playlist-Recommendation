102c052d50e3d7579426c5b0233083ff977b0013
Christopher Billovits <cjbillov@stanford.edu>
2015-12-05 22:30:08 -0800

    Scraper updated to use curl.

--- data/scripts/scraper.py
+++ data/scripts/scraper.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import signal
 import os
+import re
 # Fake threading only used for network I/O
 import multiprocessing.dummy as mp
 
@@ -21,32 +22,34 @@ _api_entry = "http://api.soundcloud.com"
    Does nothing yet.
 """
 
-def download_from_uri(arg):
+def download_from_uri(arg, sizelimit=10*1024**2):
     """ Start method for the threads to download to file """
 
     uri, title = arg
-    
-    filepath = ur"../raw/{0}".format(unicode( '_'.join(title.split(' '))))
-    
-    cmd = "wget -O {0}.mp3 {1}?client_id={2}".format(filepath, uri, _public_key)
-        
-    proc = subprocess.Popen(shlex.split(cmd))
-    proc.wait()
+    title = re.sub(r"(\'|\")", "", title)
+    filepath = '_'.join(title.split(' ')) + ".mp3"
+    if os.path.exists(filepath):
+        print "ALREADY EXISTS"
+        return
+
+    cmd = "curl -L --max-filesize {3} -o {0} {1}?client_id={2}".format(filepath, uri, _public_key, sizelimit)
+
+    proc = subprocess.call(shlex.split(cmd))
     print "DONE"
                       
 def get_tracks(pool, count, query, gen=None):
-    print _public_key
+    print gen
     client = soundcloud.Client(client_id=_public_key)
-    tracks = client.get('/tracks', q=query, genre=gen, limit=count)
+    tracks = client.get('/tracks', q=query, genres=gen, limit=count)
     
     args = [(track.stream_url, track.title.encode("utf-8")) for track in tracks if track.stream_url]
     for arg in args:
         print "{0}: Stream from {1}".format(arg[1],
                                               arg[0])
 
-    
     try:
         pool.map_async(download_from_uri, args).get()
+
         pool.close()
         print "POOL CLOSING"
         pool.join()    
@@ -59,8 +62,8 @@ def get_tracks(pool, count, query, gen=None):
 if __name__ == '__main__':
     
     # default query
-    query = "Blue Stahli"
-    genre = None
+    query = None
+    genres = ["Dance & EDM"]
     if len(sys.argv) > 1:
         query = sys.argv[1]
     with open("API_KEY", 'r') as f:
@@ -69,7 +72,7 @@ if __name__ == '__main__':
     # Change to the "raw" directory for wget a
     # for more seamless use of --content-disposition
     os.chdir("../raw")
-    get_tracks (mp.Pool(4), 10, query, genre)
+    get_tracks (mp.Pool(4), 200, query, ",".join(genres))
     print "DONE ALL"
     os.chdir("../scripts")
     
